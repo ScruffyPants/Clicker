d1fe465afa5124d4fd21ecae8d23fdbefbc48a82
Greta Kerpiškytė <gretakerp@gmail.com>
2017-10-22 21:25:13 +0300

    Added functionality

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-var counter = 0;
-function clicked(){
-  document.getElementById("info").innerHTML = ++counter;
+var woodCounter = 0;
+var stoneCounter = 0;
+var foodCounter = 0;
+var metalCounter = 0;
+var troopsCounter = 0;
+
+var counter = new Array(6).fill(0);
+
+var people = {};
+var resources = {wood:0, stone:0, food:0, metal:0, weapons:0};
+
+function clicked(clickedId){
+	
+	counter[clickedId]++;
+	alert(counter[clickedId]);
+  //document.getElementById("info").innerHTML = ++counter;
   console.log('press');
   return "test";
 }
+
