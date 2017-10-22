db9a0a7c28e038669af9ed19b432e3df52692ec7
ScruffyPants <Vilius.vaiciunas12@gmail.com>
2017-10-22 19:47:26 +0300

    Fixed Clicking
    Cannot have function named click

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
-function click(){
+function clicked(){
   document.getElementById("info").innerHTML="1";
   console.log('press');
   return "test";
 }
-
-$("#button").click(function(){
-  alert('test');
-});
-
-$("#container").on("click", function(){
-  alert("test");
-});
