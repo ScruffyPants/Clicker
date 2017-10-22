9ac89ceaecc6c1485d248de3490e625c4672f86a
Greta Kerpiškytė <gretakerp@gmail.com>
2017-10-22 21:44:27 +0300

    stuff

--- script.js
+++ script.js
@@ -8,10 +8,9 @@ var counter = new Array(6).fill(0);
 
 var people = {woodcutters:0, miners:0, troops:0};
 var resources = {wood:0, stone:0, food:0, metal:0, weapons:0};
+var houses = {small:0, large:0};
 
 function clicked(clickedId){
-
-  //document.getElementById("info").innerHTML = ++counter;
 
   resources[clickedId]++;
   document.getElementById("info").innerHTML = resources[clickedId];
